9128e9083993f95b430587e91097f94cbdfbe642
Aleksandra Szychlinska <szychlinska@gmail.com>
2021-08-01 20:49:04 +0200

    full intro

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const langSpan = document.querySelector('.lang-span');
 const langArray = ['', 'PHP', 'JavaScript', 'CSS'];
+const postSpan = document.querySelector('.post-span');
+const postArray = ['', 'Web Designer', 'Frontend Developer'];
+const passtimeSpan = document.querySelector('.passtime-span');
+const passtimeArray = ['', 'black&white photograpy', 'strawberries', 'flip-flops', 'strong tea'];
 let counter = 1;
 
-langSpan.innerText = langArray[counter];
+// langSpan.innerText = langArray[counter];
 
-langSpan.addEventListener('click', () => {
-    counter += 1;
-    langSpan.innerText = langArray[counter];
-    if (counter === langArray.length-1) {
-        counter = 0;
-    }
-    
-})
+function spanChanger (span, array) {
+    span.innerText = array[counter];
+    span.addEventListener('click', () => {
+        counter += 1;
+        span.innerText = array[counter];
+        if (counter === array.length-1) {
+            counter = 0;
+        }
+    })
+}
+
+spanChanger(langSpan, langArray);
+spanChanger(postSpan, postArray);
+spanChanger(passtimeSpan, passtimeArray)
 
 
