e75302f0638e77933ab2dc1480a171d24fe57019
Alex <67001075+szych111@users.noreply.github.com>
2024-03-20 21:51:14 +0100

    layout WIP Changes to be committed: new file:   first-version.html modified:   index.html modified:   main.js new file:   scss/_utility-classes.scss new file:   scss/old-styles.css new file:   scss/old-styles.css.map new file:   scss/old-styles.scss modified:   scss/style.css modified:   scss/style.css.map modified:   scss/style.scss

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
-const langSpan = document.querySelector('.lang-span');
-const langArray = ['JavaScript', 'PHP', 'CSS'];
-const postSpan = document.querySelector('.post-span');
-const postArray = ['Frontend Developer', 'Web Designer'];
-const passtimeSpan = document.querySelector('.passtime-span');
-const passtimeArray = ['black&white photograpy', 'strawberries', 'flip-flops'];
-const futureSpan = document.querySelector('.future-span');
-const futureArray = ['programming-guru', 'triathlonist', 'gardener'];
+const passionsWrapper = document.querySelector("[data-span=passions]");
+const virtuesWrapper1 = document.querySelector("[data-span=virtues-1]");
+const virtuesWrapper2 = document.querySelector("[data-span=virtues-2]");
+const placesWrapper = document.querySelector("[data-span=places]");
+const passions = ["user experience", "user interface", "UX/UI"];
+const virtues = ["disciplined", "creative"];
+const places = [
+  "in America",
+  "in Western Hemisphere",
+  "somewhere where it's earlier",
+];
+const future = ["programming-guru", "triathlonist", "gardener"];
 
+console.log(virtuesWrapper1);
 let counter = 0;
 
-window.addEventListener('DOMContentLoaded', () => {
-    langSpan.textContent = langArray[counter];
-    postSpan.textContent = postArray[counter];
-    passtimeSpan.textContent = passtimeArray[counter];
-    futureSpan.textContent = futureArray[counter];
-
-  })
+window.addEventListener("DOMContentLoaded", () => {
+  passionsWrapper.textContent = passions[counter];
+  virtuesWrapper1.textContent = virtues[counter];
+  virtuesWrapper2.textContent = virtues[counter + 1];
+  placesWrapper.textContent = places[counter];
+});
 
 function spanChanger(span, array) {
-    span.addEventListener('click', () => {
-        counter++;
-        if (counter > array.length - 1) {
-            counter = 0;
-        }
-        span.textContent = array[counter]
-    })
+  span.addEventListener("click", () => {
+    counter++;
+    if (counter > array.length - 1) {
+      counter = 0;
+    }
+    span.textContent = array[counter];
+  });
 }
 
-spanChanger(langSpan, langArray);
-spanChanger(postSpan, postArray);
-spanChanger(passtimeSpan, passtimeArray);
-spanChanger(futureSpan, futureArray)
+spanChanger(passionsWrapper, passions);
+spanChanger(virtuesWrapper1, virtues);
+spanChanger(virtuesWrapper2, virtues);
+spanChanger(placesWrapper, places);
 
 ///--- slider ---///
 
@@ -42,44 +46,44 @@ const prevBtn = document.querySelector(".carousel__btn--left");
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
-}
+  slide.style.left = slideWidth * index + "px";
+};
 
 slides.forEach(setSlidePosition);
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-    currentSlide.classList.remove("current-slide");
-    targetSlide.classList.add("current-slide");
-}
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
 
 const hideShowArrows = (slides, prevBtn, nextBtn, targetIndex) => {
-    if(targetIndex === 0) {
-        prevBtn.classList.add("is-hidden");
-        nextBtn.classList.remove("is-hidden");
-    } else if (targetIndex === slides.length - 1) {
-        nextBtn.classList.add("is-hidden");
-        prevBtn.classList.remove("is-hidden");
-    } else {
-        prevBtn.classList.remove("is-hidden");
-        nextBtn.classList.remove("is-hidden");
-    }
-}
+  if (targetIndex === 0) {
+    prevBtn.classList.add("is-hidden");
+    nextBtn.classList.remove("is-hidden");
+  } else if (targetIndex === slides.length - 1) {
+    nextBtn.classList.add("is-hidden");
+    prevBtn.classList.remove("is-hidden");
+  } else {
+    prevBtn.classList.remove("is-hidden");
+    nextBtn.classList.remove("is-hidden");
+  }
+};
 
-nextBtn.addEventListener('click', e => {
-    const currentSlide = track.querySelector(".current-slide");
-    const nextSlide = currentSlide.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide)
-    
-    moveToSlide(track, currentSlide, nextSlide)
-    hideShowArrows(slides, prevBtn, nextBtn, nextIndex)
-})
+nextBtn.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const nextIndex = slides.findIndex((slide) => slide === nextSlide);
 
-prevBtn.addEventListener('click', e => {
-    const currentSlide = track.querySelector(".current-slide");
-    const prevSlide = currentSlide.previousElementSibling;
-    const prevIndex = slides.findIndex(slide => slide === prevSlide)
+  moveToSlide(track, currentSlide, nextSlide);
+  hideShowArrows(slides, prevBtn, nextBtn, nextIndex);
+});
 
-    moveToSlide(track, currentSlide, prevSlide)
-    hideShowArrows(slides, prevBtn, nextBtn, prevIndex)
-})
+prevBtn.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+  const prevIndex = slides.findIndex((slide) => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide);
+  hideShowArrows(slides, prevBtn, nextBtn, prevIndex);
+});
