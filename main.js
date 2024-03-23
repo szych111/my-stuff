f2ba04d48378072e4b826f05c1e8fab34cad8610
Alex <67001075+szych111@users.noreply.github.com>
2024-03-23 22:06:36 +0100

    projects center WIP

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-$(document).ready(function () {
-  $(".projects-slider").slick({
-    // autoplay: true,
-    slidesToShow: 1,
-    prevArrow: $(".slider__button.prev"),
-    nextArrow: $(".slider__button.next"),
-  });
-});
-
 const passionsWrapper = document.querySelector("[data-span=passions]");
 const virtuesWrapper1 = document.querySelector("[data-span=virtues-1]");
 const virtuesWrapper2 = document.querySelector("[data-span=virtues-2]");
 const placesWrapper = document.querySelector("[data-span=places]");
+const nonsenseWrapper = document.querySelector("[data-span=nonsense]");
+const senseWrapper = document.querySelector("[data-span=sense]");
 const passions = ["user experience", "user interface", "UX/UI"];
 const virtues = ["disciplined", "creative"];
-const places = [
-  "in America",
-  "in Western Hemisphere",
-  "somewhere where it's earlier",
-];
+const places = ["where it's earlier", "where it's sunnier", "else"];
 const future = ["programming-guru", "triathlonist", "gardener"];
 
 let counter = 0;
@@ -27,6 +16,7 @@ window.addEventListener("DOMContentLoaded", () => {
   virtuesWrapper1.textContent = virtues[counter];
   virtuesWrapper2.textContent = virtues[counter + 1];
   placesWrapper.textContent = places[counter];
+  nonsenseWrapper.classList.add("display-none");
 });
 
 function spanChanger(span, array) {
@@ -36,6 +26,28 @@ function spanChanger(span, array) {
       counter = 0;
     }
     span.textContent = array[counter];
+    if (span === virtuesWrapper1 || span === virtuesWrapper2) {
+      if (
+        (virtuesWrapper1.textContent === array[1] &&
+          virtuesWrapper2.textContent === array[1]) ||
+        (virtuesWrapper1.textContent === array[0] &&
+          virtuesWrapper2.textContent === array[0])
+      ) {
+        document
+          .querySelector("[data-span=nonsense]")
+          .classList.remove("display-none");
+        document
+          .querySelector("[data-span=sense]")
+          .classList.add("display-none");
+      } else {
+        document
+          .querySelector("[data-span=sense]")
+          .classList.remove("display-none");
+        document
+          .querySelector("[data-span=nonsense]")
+          .classList.add("display-none");
+      }
+    }
   });
 }
 
@@ -43,61 +55,3 @@ spanChanger(passionsWrapper, passions);
 spanChanger(virtuesWrapper1, virtues);
 spanChanger(virtuesWrapper2, virtues);
 spanChanger(placesWrapper, places);
-
-///--- slider ---///
-
-// const track = document.querySelector(".carousel__track");
-// const slides = Array.from(track.children);
-// const nextBtn = document.querySelector(".carousel__btn--right");
-// const prevBtn = document.querySelector(".carousel__btn--left");
-
-// const slideWidth = slides[0].getBoundingClientRect().width;
-
-// const setSlidePosition = (slide, index) => {
-//   slide.style.left = slideWidth * index + "px";
-// };
-
-// slides.forEach(setSlidePosition);
-
-// const moveToSlide = (track, currentSlide, targetSlide) => {
-//   track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-//   currentSlide.classList.remove("current-slide");
-//   targetSlide.classList.add("current-slide");
-// };
-
-// const hideShowArrows = (slides, prevBtn, nextBtn, targetIndex) => {
-//   if (targetIndex === 0) {
-//     prevBtn.classList.add("is-hidden");
-//     nextBtn.classList.remove("is-hidden");
-//   } else if (targetIndex === slides.length - 1) {
-//     nextBtn.classList.add("is-hidden");
-//     prevBtn.classList.remove("is-hidden");
-//   } else {
-//     prevBtn.classList.remove("is-hidden");
-//     nextBtn.classList.remove("is-hidden");
-//   }
-// };
-
-// nextBtn.addEventListener("click", (e) => {
-//   const currentSlide = track.querySelector(".current-slide");
-//   const nextSlide = currentSlide.nextElementSibling;
-//   const nextIndex = slides.findIndex((slide) => slide === nextSlide);
-
-//   moveToSlide(track, currentSlide, nextSlide);
-//   hideShowArrows(slides, prevBtn, nextBtn, nextIndex);
-// });
-
-// prevBtn.addEventListener("click", (e) => {
-//   const currentSlide = track.querySelector(".current-slide");
-//   const prevSlide = currentSlide.previousElementSibling;
-//   const prevIndex = slides.findIndex((slide) => slide === prevSlide);
-
-//   moveToSlide(track, currentSlide, prevSlide);
-//   hideShowArrows(slides, prevBtn, nextBtn, prevIndex);
-// });
-
-// --- slick --- //
-
-// $(document).ready(function () {
-//   $(".projects").slick({});
-// });
