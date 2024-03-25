424bc23c133767da9d1152f8296e4dba9c1945e8
Alex <67001075+szych111@users.noreply.github.com>
2024-03-25 13:46:04 +0100

    inactive projects imgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ const passionsWrapper = document.querySelector("[data-span=passions]");
 const virtuesWrapper1 = document.querySelector("[data-span=virtues-1]");
 const virtuesWrapper2 = document.querySelector("[data-span=virtues-2]");
 const placesWrapper = document.querySelector("[data-span=places]");
-const nonsenseWrapper = document.querySelector("[data-span=nonsense]");
-const senseWrapper = document.querySelector("[data-span=sense]");
+//const nonsenseWrapper = document.querySelector("[data-span=nonsense]");
+//const senseWrapper = document.querySelector("[data-span=sense]");
 const passions = ["user experience", "user interface", "UX/UI"];
-const virtues = ["disciplined", "creative"];
+//const virtues = ["disciplined", "creative"];
 const places = ["where it's earlier", "where it's sunnier", "else"];
 const future = ["programming-guru", "triathlonist", "gardener"];
 
@@ -13,10 +13,10 @@ let counter = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
   passionsWrapper.textContent = passions[counter];
-  virtuesWrapper1.textContent = virtues[counter];
-  virtuesWrapper2.textContent = virtues[counter + 1];
+  //virtuesWrapper1.textContent = virtues[counter];
+  //virtuesWrapper2.textContent = virtues[counter + 1];
   placesWrapper.textContent = places[counter];
-  nonsenseWrapper.classList.add("display-none");
+  //nonsenseWrapper.classList.add("display-none");
 });
 
 function spanChanger(span, array) {
@@ -26,32 +26,32 @@ function spanChanger(span, array) {
       counter = 0;
     }
     span.textContent = array[counter];
-    if (span === virtuesWrapper1 || span === virtuesWrapper2) {
-      if (
-        (virtuesWrapper1.textContent === array[1] &&
-          virtuesWrapper2.textContent === array[1]) ||
-        (virtuesWrapper1.textContent === array[0] &&
-          virtuesWrapper2.textContent === array[0])
-      ) {
-        document
-          .querySelector("[data-span=nonsense]")
-          .classList.remove("display-none");
-        document
-          .querySelector("[data-span=sense]")
-          .classList.add("display-none");
-      } else {
-        document
-          .querySelector("[data-span=sense]")
-          .classList.remove("display-none");
-        document
-          .querySelector("[data-span=nonsense]")
-          .classList.add("display-none");
-      }
-    }
+    // if (span === virtuesWrapper1 || span === virtuesWrapper2) {
+    //   if (
+    //     (virtuesWrapper1.textContent === array[1] &&
+    //       virtuesWrapper2.textContent === array[1]) ||
+    //     (virtuesWrapper1.textContent === array[0] &&
+    //       virtuesWrapper2.textContent === array[0])
+    //   ) {
+    //     document
+    //       .querySelector("[data-span=nonsense]")
+    //       .classList.remove("display-none");
+    //     document
+    //       .querySelector("[data-span=sense]")
+    //       .classList.add("display-none");
+    //   } else {
+    //     document
+    //       .querySelector("[data-span=sense]")
+    //       .classList.remove("display-none");
+    //     document
+    //       .querySelector("[data-span=nonsense]")
+    //       .classList.add("display-none");
+    //   }
+    // }
   });
 }
 
 spanChanger(passionsWrapper, passions);
-spanChanger(virtuesWrapper1, virtues);
-spanChanger(virtuesWrapper2, virtues);
+// spanChanger(virtuesWrapper1, virtues);
+// spanChanger(virtuesWrapper2, virtues);
 spanChanger(placesWrapper, places);
